17f6d764fcfcbbd5bc76a33a3c3687323f573b77
Kukoy <kdl06@live.fr>
2018-11-26 17:30:50 +0100

    Finition de la classe "BruteRasterImage"
    - Creation de la méthode "createRepresentation"
    - Implementation des Getter / Setter

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -31,4 +31,52 @@ public class BruteRasterImage implements Image {
             }
         }
     }
+
+
+    public void createRepresentation() {
+        colors = new Color[width][height];
+    }
+
+    public void setPixelColor(Color color, int x, int y){
+        colors[x][y] = color;
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return colors[x][y];
+    }
+
+    public void setPixelsColor(Color[][] pixels) {
+        for(int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.colors[i][j] = pixels[i][j];
+            }
+        }
+    }
+
+    private void setPixelsColor(Color color) {
+        for(int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.colors[i][j] = color;
+            }
+        }
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    protected int setWidth(int width) {
+        return this.width = width;
+    }
+
+    protected int setHeight(int height) {
+        return this.height = height;
+    }
 }
